16f983cfd0b0fb4b1b124d381973c2ec1d085b0d
agustin mateos <agustinrotondomateos@gmail.com>
2023-02-22 23:10:02 -0300

    multer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import express from "express"
 import productRouter from "./router/product.routes.js"
 import cartRouter from "./router/carts.routes.js"
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const app=express()
-const PORT=8080
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-app.use("/products",productRouter)
-app.use("/cart",cartRouter)
+    const app=express()
+    const PORT=8080
+//midelwares
+    app.use(express.json())
+    app.use(express.urlencoded({extended:true}))
+    
+//routes
+    app.use("/static",express.static(__dirname+'/public'))
+    app.use("/products",productRouter)
+    app.use("/cart",cartRouter)
 
 
 app.listen(PORT,()=>{
